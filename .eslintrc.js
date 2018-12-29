8de188a30f5d941a2329a226dccb339fbc9144bc
Andrew <you@example.com>
2018-12-29 03:04:46 +0000

    Refactor to ES6

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  "extends": "eslint-config-airbnb-base",
+  "parser": "babel-eslint",
+  "rules": {
+    "strict": 0,
+    "linebreak-style": 0,
+    "import/no-unresolved": 0
+  },
+  "globals": {
+    "atom": true,
+    "describe": true,
+    "it": true,
+    "beforeEach": true,
+    "expect": true
+  }
+}
