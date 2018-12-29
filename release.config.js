7a458c023977f1af675d8b3381d1e12c7180a38e
Andrew Jackson <andydjackson@gmail.com>
2018-12-29 18:28:43 +0000

    fix: try and fix semantic release

--- release.config.js
+++ release.config.js
@@ -2,11 +2,8 @@ module.exports = {
   plugins: [
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
-    ['@semantic-release/github', {
-      assets: [
-        { path: 'package.json', label: 'package.json' },
-      ],
-    }],
+    '@semantic-release/github',
     '@semantic-release/apm',
+    ['@semantic-release/git', { message: `chore(release): \${nextRelease.version} [skip ci]`}],
   ],
 };
