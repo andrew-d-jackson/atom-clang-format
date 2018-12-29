8e6282b53b5a69a4778a931abedacb8423d5ec6e
Andrew Jackson <andydjackson@gmail.com>
2018-12-29 18:38:52 +0000

    fix: lint

--- release.config.js
+++ release.config.js
@@ -1,9 +1,11 @@
+/* eslint-disable */
+
 module.exports = {
   plugins: [
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
     '@semantic-release/github',
     '@semantic-release/apm',
-    ['@semantic-release/git', { message: 'chore(release): \${nextRelease.version} [skip ci]' }],
+    ['@semantic-release/git', { message: 'chore(release): ${nextRelease.version} [skip ci]' }],
   ],
 };
